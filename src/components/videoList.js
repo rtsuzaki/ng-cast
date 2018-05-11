b6bf956544a0aafc153b7f9d2a7208a52dcf62c7
rtsuzaki <rtsuzaki10@gmail.com>
2018-05-11 15:57:29 -0700

    initial commit completed video list tests

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,9 @@
 angular.module('video-player')
-.component('videoList', {
-  // TODO
-});
+  .component('videoList', {
+  
+    controller: function() {
+      this.videos = window.exampleVideoData;
+      this.onClick = function() {};
+    },
+    templateUrl: 'src/templates/videoList.html',
+  });
